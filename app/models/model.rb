da8cfca171e50aaeff2492c8e8056a8bf00c5294
alexabotello <abotello20@yahoo.com>
2020-11-18 12:41:26 -0600

    commit tree 7fae43bb6fefdac95542bb94f61699a909484849

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,9 @@
-class Model
-
-end
+class Dog
+    attr_writer :name, :breed, :age
+    
+    def initialize (name, breed, age)
+        @name=name
+        @breed=breed
+        @age=age
+    end 
+end 
